b8a0f740ed955ce921ced6c25f705246533c636d
BuzkoYaroslav <yarik10.98@gmail.com>
2016-03-14 18:51:05 +0200

    Add 2 methods in class.

--- lab1/lab1/SearchWord.cs
+++ lab1/lab1/SearchWord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,37 @@ namespace searchSentencesWithSomeWord
         public string fileName { get; private set; }
         public string filter { get; private set; }
 
+        private void ReadAllSentences()
+        {
+            StreamReader strRead = new StreamReader(fileName);
+            string currentSentence = null;
+            string buffer;
+            while (!strRead.EndOfStream)
+            {
+                buffer = strRead.ReadLine();
+                while (buffer.IndexOf(spliter) != -1)
+                {
+                    currentSentence += buffer.Substring(0, buffer.IndexOf(spliter) + 1);
+                    buffer = buffer.Substring(buffer.IndexOf(spliter) + 1);
+                    buffer = buffer.TrimStart();
+                    allSentences.Add(currentSentence);
+                    currentSentence = null;
+                }
+                currentSentence = buffer;
+            }
+            if (allSentences == null)
+                throw new Exception("File has no sentence!");
+        }
+        private void SearchSentences()
+        {
+            foreach (string sentece in allSentences)
+            {
+                if (sentece.ToLower().Contains(filter))
+                {
+                    filteredSentences.Add((string)sentece.Clone());
+                }
+            }
+        }
         private void WriteSeveralSentences(List<string> sentences)
         {
             foreach (string sentence in sentences)
